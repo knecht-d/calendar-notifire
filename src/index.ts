bad73d474c5ee8409f57552c53ef7ce75772ed21
Daniel Knecht <daniel.knecht@t-onine.de>
2020-01-12 17:16:44 +0100

    Telegram with get and post

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import { get, post } from "./interfaces/http";
+const token = "<TOKEN>"; // TODO: Enter Telegram Bot Token
 
-get("https://jsonplaceholder.typicode.com/posts/101")
-    .then(console.info)
-    .catch(console.error);
+// @ts-ignore
+function getUpdate() {
+    const method = "getUpdates";
+    get(`https://api.telegram.org/bot${token}/${method}`)
+        .then(console.info)
+        .catch(console.error);
+}
 
-const data = {
-    title: "foo",
-    body: "bar",
-    userId: 101,
-};
+// @ts-ignore
+function sendMessage() {
+    const method = "sendMessage";
+    const data = {
+        chat_id: 42, // TODO: Enter proper chat id (see response of getUpdate())
+        text: "Test",
+    };
+    post(`https://api.telegram.org/bot${token}/${method}`, data)
+        .then(console.info)
+        .catch(console.error);
+}
 
-post("https://jsonplaceholder.typicode.com/posts", data)
-    .then(console.info)
-    .catch(console.error);
+getUpdate();
+sendMessage();
