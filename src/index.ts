a9307fdcba493800a7f91bf02bb15d66112dbdba
Daniel Knecht <daniel.knecht@t-onine.de>
2020-03-08 21:55:34 +0100

    Use async await

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,32 @@
 /* istanbul ignore file */
 import { get, post } from "./interfaces/http";
-const token = "<TOKEN>"; // TODO: Enter Telegram Bot Token
+const token = "<token>"; // TODO: Enter Telegram Bot Token
 
-// @ts-ignore
-function getUpdate() {
+async function getUpdate() {
     const method = "getUpdates";
-    get(`https://api.telegram.org/bot${token}/${method}`)
-        .then(console.info)
-        .catch(console.error);
+    try {
+        const result = await get(`https://api.telegram.org/bot${token}/${method}`);
+        console.info(result);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-// @ts-ignore
-function sendMessage() {
+async function sendMessage() {
     const method = "sendMessage";
     const data = {
         chat_id: 42, // TODO: Enter proper chat id (see response of getUpdate())
         text: "Test",
     };
-    post(`https://api.telegram.org/bot${token}/${method}`, data)
-        .then(console.info)
-        .catch(console.error);
+    try {
+        const result = await post(`https://api.telegram.org/bot${token}/${method}`, data);
+        console.info(result);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
+// tslint:disable-next-line: no-floating-promises
 getUpdate();
+// tslint:disable-next-line: no-floating-promises
 sendMessage();
