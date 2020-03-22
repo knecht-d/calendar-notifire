6925d1f8644a05d973eaee1208c21dd49ccf0741
Daniel Knecht <daniel.knecht@t-onine.de>
2020-03-22 21:32:41 +0100

    Ical with recurrence - without exception

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 /* istanbul ignore file */
-import { get, post } from "./interfaces/http";
+import { readFileSync } from "fs";
+import * as ICAL from "ical.js";
 
-get("https://jsonplaceholder.typicode.com/posts/101")
-    .then(console.info)
-    .catch(console.error);
+try {
+    const data = readFileSync(__dirname + "/../assets/Example2.ics").toString();
+    console.info(data);
+    const jCalData = ICAL.parse(data);
+    const comp = new ICAL.Component(jCalData);
 
-const data = {
-    title: "foo",
-    body: "bar",
-    userId: 101,
-};
+    // Fetch the VEVENT part
+    const vevents = comp.getAllSubcomponents("vevent");
+    const events = vevents.map(e => new ICAL.Event(e));
 
-post("https://jsonplaceholder.typicode.com/posts", data)
-    .then(console.info)
-    .catch(console.error);
+    events.forEach(e => {
+        if (e.isRecurring()) {
+            const iterator = e.iterator();
+            let next = iterator.next();
+            while (next) {
+                const n = e.getOccurrenceDetails(next);
+                console.info(e.summary, n.startDate.toJSDate(), (e as any).recurrenceId);
+                next = iterator.next();
+            }
+        } else {
+            console.info(e.summary, e.startDate.toJSDate(), (e as any).recurrenceId);
+        }
+    });
+} catch (err) {
+    console.error(err);
+}
