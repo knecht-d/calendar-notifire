0606429be0fffb6c35e9e4e54f90b83282055abd
Daniel Knecht <daniel.knecht@live.de>
2020-05-10 14:30:58 +0200

    Fix Mock Timer

--- test/mocks/external/MockTimer.ts
+++ test/mocks/external/MockTimer.ts
@@ -2,6 +2,7 @@ import { AbstractTimer } from "../../../src/external";
 
 export class MockTimer extends AbstractTimer {
     setTrigger = jest.fn();
+    stopTrigger = jest.fn();
 
     fireTrigger(id: string) {
         this.triggerReceiver!.trigger(id);
