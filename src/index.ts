5ecc6cf18c4eff4aa8cc1e5d87f87362eff30eee
Daniel Knecht <daniel.knecht@live.de>
2020-07-12 02:41:11 +0200

    Read Assistant Config from file

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,92 @@
+import { existsSync, readFileSync } from "fs";
 import { Builder, GenericFactory } from "./creation";
-import { CronTimer, LogLevels, SimpleFileStorage, TelegramChat, WebCalendar } from "./external";
+import {
+    AbstractChat,
+    ConsoleChat,
+    CronTimer,
+    LogLevels,
+    SimpleFileStorage,
+    TelegramChat,
+    WebCalendar,
+} from "./external";
+
+function toLogLevel(input: string): LogLevels {
+    const map: { [input: string]: LogLevels } = {
+        error: LogLevels.error,
+        warn: LogLevels.warn,
+        info: LogLevels.info,
+        verbose: LogLevels.verbose,
+        debug: LogLevels.debug,
+    };
+    return map[input];
+}
 
 (async () => {
-    const _staticEvents = [
-        {
-            title: "Event1",
-            start: new Date(2020, 4, 16, 12, 30, 0, 0),
-            end: new Date(2020, 4, 16, 13, 30, 0, 0),
-        },
-        {
-            title: "Event2",
-            start: new Date(2020, 4, 16, 13, 30, 0, 0),
-            end: new Date(2020, 4, 16, 14, 30, 0, 0),
-        },
-        {
-            title: "Event3",
-            start: new Date(2020, 4, 16, 14, 30, 0, 0),
-            end: new Date(2020, 4, 16, 15, 30, 0, 0),
-        },
-        {
-            title: "Event4",
-            start: new Date(2020, 4, 16, 15, 30, 0, 0),
-            end: new Date(2020, 4, 16, 16, 30, 0, 0),
-        },
-        {
-            title: "Event5",
-            start: new Date(2020, 4, 16, 16, 30, 0, 0),
-            end: new Date(2020, 4, 16, 17, 30, 0, 0),
-        },
-        {
-            title: "Event6",
-            start: new Date(2020, 4, 16, 17, 30, 0, 0),
-            end: new Date(2020, 4, 16, 18, 30, 0, 0),
-        },
-        {
-            title: "Event7",
-            start: new Date(2020, 4, 16, 18, 30, 0, 0),
-            end: new Date(2020, 4, 16, 19, 30, 0, 0),
-        },
-        {
-            title: "Event8",
-            start: new Date(2020, 4, 16, 19, 30, 0, 0),
-            end: new Date(2020, 4, 16, 20, 30, 0, 0),
-        },
-        {
-            title: "Event9",
-            start: new Date(2020, 4, 16, 20, 30, 0, 0),
-            end: new Date(2020, 4, 16, 21, 30, 0, 0),
-        },
-        {
-            title: "Event10",
-            start: new Date(2020, 4, 16, 21, 30, 0, 0),
-            end: new Date(2020, 4, 16, 22, 30, 0, 0),
-        },
-    ];
-    _staticEvents.slice();
-    // let factory = new LocalFactory();
-    // const factory = new GenericFactory({
-    //     calendar: WebCalendar,
-    //     storage: SimpleFileStorage,
-    //     chat: ConsoleChat,
-    //     timer: TestSetIntervalTimer,
-    // });
-    const factory = new GenericFactory({
-        calendar: WebCalendar,
-        storage: SimpleFileStorage,
-        chat: TelegramChat,
-        timer: CronTimer,
-    });
-    const builder = new Builder(factory);
-    const { chat } = await builder.build({
-        calendar: {
-            url: "***REMOVED***",
-        },
-        storage: {
-            file: "data/chats.json",
-        },
-        chatData: {
-            botToken: process.env.BOT_TOKEN || "***REMOVED***",
-        },
-        loggger: {
-            level: LogLevels.info,
-        },
-    });
+    const setupFile = `${__dirname}/../data/setup.json`;
+    if (!existsSync(setupFile)) {
+        console.error(`Missing ${setupFile}`);
+        return;
+    }
+
+    const rawData = readFileSync(setupFile, { encoding: "utf8" });
+    const setupData = JSON.parse(rawData);
+
+    if (!setupData.calendarURL) {
+        console.error(`Missing calendarURL in ${setupFile}`);
+        return;
+    }
+
+    let chat: AbstractChat<unknown>;
+    if (setupData.telegram) {
+        const factory = new GenericFactory({
+            calendar: WebCalendar,
+            storage: SimpleFileStorage,
+            chat: TelegramChat,
+            timer: CronTimer,
+        });
+        const builder = new Builder(factory);
+        chat = (
+            await builder.build({
+                calendar: {
+                    url: setupData.calendarURL,
+                },
+                storage: {
+                    file: setupData.chatConfig || "data/chats.json",
+                },
+                chatData: {
+                    botToken: setupData.telegram,
+                },
+                loggger: {
+                    level: toLogLevel(setupData.logLevel) ?? LogLevels.info,
+                },
+            })
+        ).chat;
+    } else {
+        const factory = new GenericFactory({
+            calendar: WebCalendar,
+            storage: SimpleFileStorage,
+            chat: ConsoleChat,
+            timer: CronTimer,
+        });
+        const builder = new Builder(factory);
+        chat = (
+            await builder.build({
+                calendar: {
+                    url: setupData.calendarURL,
+                },
+                storage: {
+                    file: setupData.chatConfig || "data/chats.json",
+                },
+                chatData: {
+                    chatId: "Console",
+                    userId: "ConsoleUser",
+                },
+                loggger: {
+                    level: toLogLevel(setupData.logLevel) ?? LogLevels.info,
+                },
+            })
+        ).chat;
+    }
 
     await chat.start();
 })().catch(console.error);
